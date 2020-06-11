b3e530fad3710439de916970fed3b7bde51fef06
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-06-11 00:44:01 -0700

    lab-08

--- server.js
+++ server.js
@@ -1,136 +1,113 @@
-'use strict';
+'use strict'
+
 const express = require('express');
-const app = express();
-require('dotenv').config();
 const cors = require('cors');
-app.use(cors());
 const superagent = require('superagent');
+
+const pg = require('pg');
+
+require('dotenv').config();
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+const ERROR404 = 'The page does not exist.';
+
+const sqlClient = new pg.Client(process.env.DATABASE_URL);
+sqlClient.on('error', err => console.log(err));
 
 app.get('/location', (request, response) => {
   let city = request.query.city;
-  let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`;
-  
-  superagent.get(url)
-    .then(resultsFromSuperAgent => {
-      let finalObj = new Location(city, resultsFromSuperAgent.body[0]);
-      response.status(200).send(finalObj);
+  let sqlQuery = `SELECT search_query, formatted_query, latitude, longitude FROM cities WHERE search_query = $1;`;
+  const safeValues = [city];
+
+  sqlClient.query(sqlQuery, safeValues)
+    .then(sqlResult => {
+      console.log(sqlResult.rows);
+
+      if (!sqlResult.search_query) {
+        console.log('call superagent');
+        let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`;
+        superagent.get(url)
+          .then(resultsFromSuperAgent => {
+            let returnObj = new Location(city, resultsFromSuperAgent.body[0])
+            let sqlQuery1 = `INSERT INTO cities (search_query, formatted_query, latitude,
+              longitude) VALUES ($1, $2, $3, $4);`
+            const safeValue = [city,
+              returnObj.formatted_query,
+              returnObj.latitude,
+              returnObj.longitude];
+
+            sqlClient.query(sqlQuery1, safeValue)
+              .then(sqlResult => {
+                console.log(sqlResult.rows)
+              }).catch(err => console.log(err));
+
+            response.status(200).send(returnObj);
+            console.log(returnObj);
+          }).catch(err => console.log(err));
+      } else {
+        console.log('send the sqlResult back to the client');
+      }
     })
-  })
+    .catch(err => console.log(err));
+})
 
 app.get('/weather', (request, response) => {
-    let search_query = request.query.search_query;
-    console.log('stuff I got from the front end on the weather route', search_query);
-  
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${search_query}&key=${process.env.WEATHER_API_KEY}&days=8`;
-  
-    superagent.get(url)
-      .then(resultsFromSuperAgent => {
-      let weatherResult = resultsFromSuperAgent.body.data.map(day => {
-      return new Weather(day); 
-      }) 
-      response.status(200).send(weatherResult);
-      }).catch(err => console.log(err));
+  let city = request.query.search_query;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${process.env.WEATHER_API_KEY}&days=8`;
+  superagent.get(url)
+    .then(resultsFromSuperAgent => {
+      const weatherArray = resultsFromSuperAgent.body.data.map(day => {
+        return new Weather(day);
+      })
+      response.status(200).send(weatherArray);
+    }).catch(err => console.log(err))
 })
 
 app.get('/trails', (request, response) => {
-  const lat = request.query.latitude;
-  const lon = request.query.longitude;
-  const key = process.env.TRIAL_API_KEY;
-  const url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${key}`;
+  let {latitude, longitude} = request.query;
+  let url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${key}`;
   
   superagent.get(url)
-  .then(resultsFromSuperAgent => {
-    const data = resultsFromSuperAgent.body.trails;
-    const results = data.map(item => new Trail(item));
-    console.log(results);
-    response.status(200).send(results);
-  }).catch(err=> console.log(err));
+    .then(resultsFromSuperAgent => {
+      const trailArr = resultsFromSuperAgent.body.trails.map(trail => {
+        return new Trail(trail);
+      })
+      response.status(200).send(trailArr);
+    }).catch(err => console.log(err));
 })
 
-function Trail(obj){
-  this.name = obj.name;
-  this.location = obj.location;
-  this.length = obj.length;
-  this.stars = obj.stars;
-  this.star_votes = obj.starVotes;
-  this.summary = obj.summary;
-  this.trail_url = obj.url;
-  this.conditions = obj.conditionStatus;
-  this.condition_date = obj.conditionDate;
-  this.condition_time = obj.conditionDate;
-}
-
-const PORT = process.env.PORT || 3001;
-
-function Weather(obj){
-    this.forecast = obj.weather.description;
-    this.time = obj.valid_date;
-}
-   
-function Location(searchQuery, obj){
+function Location(searchQuery, obj) {
   this.search_query = searchQuery;
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
 }
 
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
+function Weather(obj) {
+  this.forecast = obj.weather.description;
+  this.time = obj.valid_date;
+}
+
+function Trail(obj) {
+  this.name = obj.name;
+  this.location = obj.location;
+  this.length = obj.length;
+  this.stars = obj.stars;
+  this.star_votes = obj.starVotes;
+  this.trail_url = obj.url;
+  this.conditions = `${obj.conditionStatus} ${obj.conditionDetails}`;
+  this.condition_date = obj.conditionDate.slice(0, 10);
+  this.condition_date = obj.conditionDate.slice(12, 19);
+}
+
+app.get('*', (request, response) => {
+  response.status(404).send(ERROR404);
+});
+
+sqlClient.connect().then( () => {
+  app.listen(PORT, () => {
+    console.log(`listening on PORT: ${PORT}`);
+  })
 })
-
-
-
-
-
-// 'use strict';
-
-// const express = require('express');
-// const cors = require('cors');
-// const superagent = require('superagent');
-// require('dotenv').config();
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(cors());
-
-// app.get('/location', (request, response) => {
-//   let city = request.query.city;
-
-//   let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEO_DATA_API_KEY}&q=${city}&format=json`;
-
-//   superagent.get(url)
-//     .then(resultsFromSuperAgent => {
-//       let finalObj = new Location(city, resultsFromSuperAgent.body[0]);
-
-//       response.status(200).send(finalObj);
-//     })
-
-
-//   // go to the web and get real information using superagent
-//   // run that information through the constructor
-//   // send it to the front end
-// })
-
-// app.get('/weather', (request, response) => {
-//   let search_query = request.query.search_query;
-//   console.log('stuff I got from the front end on the weather route', search_query);
-
-//   let url = `https://api.weatherbit.io/v2.0/current?city=${search_query}&key=${process.env.WEATHER_API_KEY}`;
-
-//   superagent.get(url)
-//     .then(resultsFromSuperAgent => {
-//       console.log(resultsFromSuperAgent.body);
-//     }).catch(err => console.log(err));
-// })
-
-// function Location(searchQuery, obj){
-//   this.search_query = searchQuery;
-//   this.formatted_query = obj.display_name;
-//   this.latitude = this.lat;
-//   this.longitude = this.lon;
-// }
-
-// app.listen(PORT, () => {
-//   console.log(`listening on ${PORT}`);
-// })
